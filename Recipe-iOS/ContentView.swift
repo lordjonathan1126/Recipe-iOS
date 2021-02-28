6765699c7667abf4e57672b5858e691a318a9a5f
Jonathan Ng <iamjonathanng6657@gmail.com>
2021-02-28 18:44:46 +0800

    Initial Commit

---
+++ Recipe-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recipe-iOS
+//
+//  Created by Jonathan Ng on 28/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
